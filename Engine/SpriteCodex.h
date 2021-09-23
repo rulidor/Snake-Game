2c5e1a7ea7b47197e43cb55bab0e394ccb476220
rulidor <rulidor90@gmail.com>
2021-09-23 20:31:06 +0300

    updated graphics

--- Engine/SpriteCodex.h
+++ Engine/SpriteCodex.h
@@ -4,9 +4,7 @@
 class SpriteCodex
 {
 public:
-	void DrawGameOverOriginal(int x, int y, Graphics& gfx);
 	static void DrawGameOver(int x, int y, Graphics& gfx);
-	void DrawTitleOriginal(int x, int y, Graphics& gfx);
 	static void DrawTitle(int x, int y, Graphics& gfx);
 
 
